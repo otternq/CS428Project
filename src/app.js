943b83f30b0ec9a2f87bc764556f8076ee86c874
otternq <otternq@gmail.com>
2013-02-28 16:57:48 -0800

    renaming index.html to remote.html and printing server port

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ io.configure(function () {
 });
 
 var port = process.env.PORT || 5000; // Use the port that Heroku provides or default to 5000
+console.log("Listening to port "+ port);
 
 app.listen(port);
 
