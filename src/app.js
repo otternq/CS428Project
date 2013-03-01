080c6412d27a717e509823fcb551431d886af8bc
otternq <otternq@gmail.com>
2013-02-28 18:08:36 -0800

    orientation data is now sent from remote.html to all clients

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
-  , fs = require('fs')
+  , fs = require('fs'),
+  url = require('url');
 
 io.configure(function () { 
   io.set("transports", ["xhr-polling"]); 
@@ -13,7 +14,20 @@ console.log("Listening to port "+ port);
 app.listen(port);
 
 function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
+
+  var url_parts = url.parse(req.url);
+
+  var requestPath = url_parts.pathname;
+
+  var file = "";
+
+  if (requestPath == "/remote.html") {
+    file = "/remote.html";
+  } else {
+    file = "/index.html";
+  }
+
+  fs.readFile(__dirname + file,
   function (err, data) {
     if (err) {
       res.writeHead(500);
@@ -27,7 +41,17 @@ function handler (req, res) {
 
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
+
   socket.on('cor', function (data) {
     console.log(data);
+    socket.broadcast.emit("incor", data);
   });
+
+  socket.on('tempClick', function (data) {
+    console.log("button clicked:");
+    console.log(data);
+    socket.emit("data", {click: "You clicked it!"});
+    socket.broadcast.emit('bcast', "Hi there");
+  })
+
 });
