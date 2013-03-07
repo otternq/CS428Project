7e5d960d6476f51acef3fefb24a1d53b30317e41
otternq <otternq@gmail.com>
2013-03-06 19:34:47 -0800

    initial commit

--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@ var app = require('http').createServer(handler)
   , fs = require('fs'),
   url = require('url');
 
-io.configure(function () { 
+/*io.configure(function () { 
   io.set("transports", ["xhr-polling"]); 
   io.set("polling duration", 10); 
-});
+});*/
 
 var port = process.env.PORT || 5000; // Use the port that Heroku provides or default to 5000
 console.log("Listening to port "+ port);
