22178edfe053a7fb60433184946180247e41522e
otternq <otternq@gmail.com>
2013-03-10 12:27:36 -0700

    the remote will now move a box around index.html
    The `step` variable defines how much the box is moved. this is a basic
    movement, and some logic needs to be implemented to have an domain of
    positions that will not move the box. Also changed static server to
    express module instead of custom HTTP handling.

--- src/app.js
+++ src/app.js
@@ -1,43 +1,21 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs'),
-  url = require('url');
+var port = process.env.PORT || 5000; // Use the port that Heroku provides or default to 5000
+
+console.log("Listening to port "+ port);
+
+var express = require('express');
+var app = express();
+var server = app.listen(port);
+var io = require('socket.io').listen(server);
 
 /*io.configure(function () { 
   io.set("transports", ["xhr-polling"]); 
   io.set("polling duration", 10); 
 });*/
 
-var port = process.env.PORT || 5000; // Use the port that Heroku provides or default to 5000
-console.log("Listening to port "+ port);
+app.configure(function(){
+  app.use(express.static(__dirname + '/public/'));
+});
 
-app.listen(port);
-
-function handler (req, res) {
-
-  var url_parts = url.parse(req.url);
-
-  var requestPath = url_parts.pathname;
-
-  var file = "";
-
-  if (requestPath == "/remote.html") {
-    file = "/remote.html";
-  } else {
-    file = "/index.html";
-  }
-
-  fs.readFile(__dirname + file,
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
 
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
