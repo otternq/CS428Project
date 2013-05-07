e4808e0d41731c0ecbb316f78417657fd31c2eb3
otternq <otternq@gmail.com>
2013-05-07 00:31:17 -0700

    #63 adding the frames for the explosion animation

--- src/public/js/libs/app/entities/bossentity.js
+++ src/public/js/libs/app/entities/bossentity.js
@@ -35,7 +35,7 @@ define([
 			this.parent(x, y, settings);
 
 			this.addAnimation("fight", [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14], 1);
-			this.addAnimation("explode", [15,16,17,18,19,20], 1);
+			this.addAnimation("explode", [15,16,17,18,19,20, 21, 22, 23, 24, 25, 26, 27, 28], 1);
 			this.setAnimationFrame(0);
 			this.animationpause = true;
 
